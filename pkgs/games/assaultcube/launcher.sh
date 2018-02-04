2a149168ac40c19fa667915302ff0b905e95723b
Bignaux Ronan <ronan@aimao.org>
2018-02-04 14:39:43 +0100

    assaultcube : fix launcher

--- pkgs/games/assaultcube/launcher.sh
+++ pkgs/games/assaultcube/launcher.sh
@@ -6,11 +6,11 @@ CUBE_DIR=/share/games/assaultcube
 case "$0" in
   assaultcube-server)
     CUBE_OPTIONS="-Cconfig/servercmdline.txt"
-    BINARYPATH = /bin/ac_client
+    BINARYPATH = /bin/ac_server
     ;;
   assaultcube)
     CUBE_OPTIONS="--home=${HOME}/.assaultcube/v1.2next --init"
-    BINARYPATH = /bin/ac_server
+    BINARYPATH = /bin/ac_client
     ;;
 esac
 
